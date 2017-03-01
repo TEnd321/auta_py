c8b41c4853a73f5030964ebf498747e5c6a6da18
TEnd321 <miskomedvecky@gmail.com>
2017-03-01 20:23:53 +0100

    Add files via upload

---
+++ auta.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import time
+import math
+import random
+
+main = tk.Tk()
+
+but1 = tk.Button(main,text="sgfhilsggjofsduh")
+but2 = tk.Button(main,text="adsfsafgsad")
+entry1 = tk.Entry(main)
+can1 = tk.Canvas(main, width=200,height=200, bg = "#FFFFFF")
+
+can1.grid(row = 1, column = 0, columnspan = 3)
+but1.pack(row = 2, column = 0)
+but1.pack(row = 2, column = 1)
+but1.pack(row = 2, column = 2)
+
+main.mainloop()
