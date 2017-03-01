6ff8d58c32a797f6ddda5b6c06fb99bfe01a7495
TEnd321 <miskomedvecky@gmail.com>
2017-03-01 19:34:11 +0000

    Add files via upload

--- auta.py
+++ auta.py
@@ -11,8 +11,8 @@ entry1 = tk.Entry(main)
 can1 = tk.Canvas(main, width=200,height=200, bg = "#FFFFFF")
 
 can1.grid(row = 1, column = 0, columnspan = 3)
-but1.pack(row = 2, column = 0)
-but1.pack(row = 2, column = 1)
-but1.pack(row = 2, column = 2)
+but1.grid(row = 2, column = 0)
+but1.grid(row = 2, column = 1)
+but1.gird(row = 2, column = 2)
 
 main.mainloop()
